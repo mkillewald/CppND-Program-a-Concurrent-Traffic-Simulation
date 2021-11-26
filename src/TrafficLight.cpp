5639b100ef6a7077563b1c5cd71608233cd0c350
Mike Killewald <mkillewald@yahoo.com>
2021-11-26 13:13:47 -0500

    FP.4a completed

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -5,7 +5,6 @@
 
 /* Implementation of class "MessageQueue" */
 
-/*
 template <typename T> T MessageQueue<T>::receive() {
   // FP.5a : The method receive should use std::unique_lock<std::mutex> and
   // _condition.wait() to wait for and receive new messages and pull them from
@@ -17,8 +16,10 @@ template <typename T> void MessageQueue<T>::send(T &&msg) {
   // FP.4a : The method send should use the mechanisms
   // std::lock_guard<std::mutex> as well as _condition.notify_one() to add a new
   // message to the queue and afterwards send a notification.
+  std::lock_guard<std::mutex> uLock(_mutex);
+  _queue.push_back(std::move(msg));
+  _cond.notify_one();
 }
-*/
 
 /* Implementation of class "TrafficLight" */
 
